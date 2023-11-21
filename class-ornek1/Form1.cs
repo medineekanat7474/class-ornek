44e35a401c62915de51bafcf75aec04dd2989817
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-11-21 10:52:49 +0300

    Add files via upload

---
+++ class-ornek1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_ornek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kitap kitap1=new Kitap();
+            kitap1.YazarAdi = "ASLI ARSLAN";
+            kitap1.Turu = "ROMAN";
+            kitap1.Sayfasayisi = 621;
+            kitap1.KitapBoyutu = 234;
+            kitap1.kitapismi = "SOKAK NÖBETÇİLERİ";
+            kitap1.YayinlananTarih = 2018;
+            label1.Text = kitap1.YazarAdi + " " + kitap1.Turu + " " + kitap1.Sayfasayisi + " " + kitap1.KitapBoyutu + " " + kitap1.Kitapİsmi + " " + kitap1.YayinlananTarih + " ";
+        }
+    }
+}
---
+++ class-ornek1/Kitap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_ornek1
+{
+    public class Kitap
+    {
+        public string YazarAdi;
+        public string Turu;
+        public int Sayfasayisi;
+        public int KitapBoyutu;
+        public string Kitapİsmi;
+        public int YayinlananTarih;
+        public string kitapismi;
+    }
+}
